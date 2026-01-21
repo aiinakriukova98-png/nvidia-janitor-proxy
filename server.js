89d400dd3be44c00bc8732c0a2c03439f5299067
aiinakriukova98-png <aiina.kriukova98@gmail.com>
2026-01-22 01:31:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const PORT = process.env.PORT || 3000;
 const NIM_API_KEY = process.env.NIM_API_KEY;
